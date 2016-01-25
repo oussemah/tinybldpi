7d4dba3775bb9cb4bf9af76bb21ac7e0a3682aa9
Oussema Harbi <oussema.elharbi@gmail.com>
2016-01-25 21:53:24 +0000

    Adding Serial Port for RPI

--- modules/searchserial.py
+++ modules/searchserial.py
@@ -10,7 +10,7 @@ def detect():
     #TODO: recognzed other OS
        
     #test if the program can use any of this ports
-    for i in ("/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyUSB0","/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4"):     
+    for i in ("/dev/ttyAMA0","/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyUSB0","/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4"):     
         #TinyBldLin Will try to open the and send a string   
         try:
             ser=serial.Serial(i,19200)
